295641ea804ac7e62e3d45877fdf5dedbd2ecce6
Jeremy Perkins <90004688+jeremyperkins@users.noreply.github.com>
2022-06-27 22:01:11 -0400

    devise steps 1-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+  devise_for :views
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #adding for devise
+  root to: "home#index"
+
+
 end
